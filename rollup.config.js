2e34d6820e93df104ac752ab1e899d3728df18ab
b0nz <elliantosarono.p@gmail.com>
2021-10-13 01:08:20 +0700

    init

---
+++ rollup.config.js
@@ -0,0 +1,82 @@
+import babel from '@rollup/plugin-babel'
+import typescript from 'rollup-plugin-typescript2';
+import external from 'rollup-plugin-peer-deps-external'
+import postcss from 'rollup-plugin-postcss-modules'
+import del from 'rollup-plugin-delete'
+import pkg from './package.json'
+
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+
+console.log('Expected Externals', [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
+  './src',
+])
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  input: {
+    index: 'src/index.tsx',
+  },
+  external: [
+    './src'
+  ],
+  output: [
+    {
+      dir: 'dist/cjs',
+      format: 'cjs',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      exports: 'named'
+    },
+    {
+      dir: 'dist/esm',
+      format: 'es',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      exports: 'named'
+    }
+  ],
+  plugin: [
+    external(),
+    typescript(),
+    nodeResolve({
+      ignoreGlobal: false,
+      include: ['node_modules/**'],
+      // eslint-disable-next-line no-undef
+      extensions,
+      // skip: keys(EXTERNALS), // <<-- skip: ['react', 'react-dom']
+    }),
+    commonjs({
+      ignoreGlobal: false,
+      include: 'node_modules/**',
+    }),
+    postcss({
+      plugins: [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+      // modules: true,
+      minimize: true,
+      sourceMap: false,
+      // extract: false,
+      // eslint-disable-next-line no-dupe-keys
+      minimize: true,
+      modules: {
+        // see generateScopedName options here
+        // https://github.com/css-modules/postcss-modules
+        generateScopedName: '[local]',
+      },
+    }),
+    babel({
+      babelHelpers: 'runtime',
+      exclude: 'node_modules/**',
+      extensions,
+    }),
+    del({ targets: ['dist/*'] }),
+  ]
+}
---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        // black
+        'evblack': '#2b2b2b',
+        // green
+        'evgreen-50': '#B1DDC7',
+        'evgreen-100': '#8ACCAB',
+        'evgreen-200': '#63BB8F',
+        'evgreen-300': '#3CAA73',
+        'evgreen-400': '#159957',
+        'evgreen-500': '#138A4E',
+        'evgreen-600': '#117A46',
+        'evgreen-700': '#0F6B3D',
+        'evgreen-800': '#0D5C34',
+        'evgreen-900': '#0B4D2C',
+        // blue
+        'evblue-50': '#B1C7DD',
+        'evblue-100': '#8AABCC',
+        'evblue-200': '#638FBB',
+        'evblue-300': '#3C73AA',
+        'evblue-400': '#3C73AA',
+        'evblue-500': '#134E8A',
+        'evblue-600': '#11467A',
+        'evblue-700': '#0F3D6B',
+        'evblue-800': '#0D345C',
+        'evblue-900': '#0B2C4D',
+        // yellow
+        'evyellow-50': '#FFF2AA',
+        'evyellow-100': '#FFEB80',
+        'evyellow-200': '#FFE455',
+        'evyellow-300': '#FFDE2B',
+        'evyellow-400': '#FFD700',
+        // red
+        'evred-50': '#F9B3B6',
+        'evred-100': '#F68E92',
+        'evred-200': '#F3686D',
+        'evred-300': '#F04249',
+        'evred-400': '#ED1C24',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}

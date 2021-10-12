c037f2203e139b6d1e25f7d5611e13d227f09ccc
b0nz <elliantosarono.p@gmail.com>
2021-10-13 02:55:36 +0700

    feat: update storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,11 @@
+// import css for san serif font styling
+import './preview.css'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  darkMode: {
+    darkClass: 'dark',
+    lightClass: 'light',
+    stylePreview: true,
   },
 }
